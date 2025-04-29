40abf110c7b514545b98b976edb1cc2fdede6211
Alex <adscarvalho@restinga.ifrs.edu.br>
2025-04-29 17:55:17 -0300

    Ajustando os links.

--- src/app/site-alex/site-alex.component.ts
+++ src/app/site-alex/site-alex.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CommandModule} from '@angular/cli/src/command-builder/command-module';
 
 @Component({
   selector: 'app-site-alex',
@@ -8,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class SiteAlexComponent {
 
+btnMenu: HTMLElement | null = null;
+menu: HTMLCollectionOf<Element> | null = null;
+overlayMenuMobile: HTMLCollectionOf<Element> | null = null;
+
+  ngOnInit() {
+    this.btnMenu = document.getElementById('btn-menu');
+    this.menu = document.getElementsByClassName('menu-mobile');
+    this.overlayMenuMobile = document.getElementsByClassName('overlay-menu-mobile');
+
+    if (this.btnMenu && this.menu && this.overlayMenuMobile) {
+      this.btnMenu.addEventListener('click', () => {
+        this.menu?.[0].classList.add('abrir-menu');
+      });
+
+      this.menu[0].addEventListener('click', () => {
+        this.menu?.[0].classList.remove('abrir-menu');
+      });
+
+      this.overlayMenuMobile[0].addEventListener('click', () => {
+        this.menu?.[0].classList.remove('abrir-menu');
+      });
+    }
+  }
 }
